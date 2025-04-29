656d32e6325ff2394704cd083250d973b186803e
Diwash <diwaskarki96@gmail.com>
2025-04-29 21:39:45 +0545

    constant

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "code", imgPath: "/images/code.svg" },
+];
